7adf28b6738a8f5bfc4a2476313ef36e7bad72c2
Hakata <arikato110011@gmail.com>
2022-04-12 19:42:15 +0700

    Edit fixed link to path='/' at NotFound.js

--- src/NotFound.js
+++ src/NotFound.js
@@ -4,7 +4,7 @@ function NotFound() {
     return <div className="App">
         <div className="content">
             <div style={{ fontSize: 30 }}>Not Found this category</div>
-            <Link to="">
+            <Link to="/">
                 <button className="btn">Fixed</button>
             </Link>
         </div>
